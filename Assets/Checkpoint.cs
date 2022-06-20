277a830b6bc33b4e09bea3279014295f6aed74f5
Casper121 <juanjosejimenez00@gmail.com>
2022-06-20 21:57:01 +0200

    Changes and some new features

--- Assets/Checkpoint.cs
+++ Assets/Checkpoint.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Checkpoint : MonoBehaviour
 {
@@ -12,6 +13,8 @@ public class Checkpoint : MonoBehaviour
         {
             FindObjectOfType<GameManager>().setRespawnPos(this.transform.position);
             Debug.Log(this.transform.position);
+            Destroy(gameObject);
+            StartCoroutine("showSavingPointText");
         }
     }
 }
